5ccbe958725a4ddeb86956df4f1744a44047b5e5
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-05-25 19:52:15 +0600

    build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+docker build -f indy-pool.dockerfile -t indy_dev_pool .
+echo "Successful process of indy_pool image building"
+docker build -f indy-dev.dockerfile -t indy_dev .
+echo "Successful process of indy_dev image building"

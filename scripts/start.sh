48da38cd2d6b7f05638a92966a74849b45fa70a8
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-05-25 20:10:24 +0600

    start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,25 @@
+if [ "$#" -eq 0 ];
+    then
+        if [[ "$(docker ps -a -f status=running | grep indy_pool)" ]];
+            then
+                echo "entering " $(pwd) "directory"
+                docker run -u root -it --net=host -p 127.0.0.1:8080:8080 -v $(pwd):/home/indy indy_dev
+            else
+                echo "entering " $(pwd) "directory"
+                docker run -u root -itd --net=host -p 127.0.0.1:9701-9708:9701-9708 indy_dev_pool
+                docker run -it -u root --net=host -p 127.0.0.1:8080:8080 -v $(pwd):/home/indy indy_dev
+        fi;
+    else
+        if [[ "$(docker ps -a -f status=running | grep indy_pool)" ]];
+            then
+                echo "entering $1 directory"
+                docker run -u root -itd --net=host -p 127.0.0.1:9701-9708:9701-9708 indy_dev_pool
+                docker run -u root -it --net=host -p 127.0.0.1:8080:8080 -v "$1":/home/indy indy_dev
+            else
+                echo "entering $1 directory"
+                docker run -u root -itd --net=host -p 127.0.0.1:9701-9708:9701-9708 indy_dev_pool
+                docker run -u root -it --net=host -p 127.0.0.1:8080:8080 -v "$1":/home/indy indy_dev
+        fi;
+fi;
+
+docker stop $(docker ps -a | grep '\indy_pool\|indy_dev' | awk '{print $1}')

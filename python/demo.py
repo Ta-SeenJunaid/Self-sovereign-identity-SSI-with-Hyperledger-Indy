18ac7442f5a8f9c750b5a214b69ccfd4e19e56b9
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-05-26 10:59:12 +0600

    Pool open

--- python/demo.py
+++ python/demo.py
@@ -1,8 +1,13 @@
 import subprocess
 import logging
 import time
+import json
 
-from src.utils import run_coroutine
+from indy import pool
+
+from indy.error import IndyError, ErrorCode
+
+from src.utils import run_coroutine, get_pool_genesis_txn_path, PROTOCOL_VERSION
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -13,6 +18,25 @@ async def run():
     output, error = process.communicate()
 
     logger.info("Code Started -> started")
+
+    pool_name = 'pool1'
+    logger.info("Open Pool Ledger: {}".format(pool_name))
+    pool_genesis_txn_path = get_pool_genesis_txn_path(pool_name)
+    pool_config = json.dumps({"genesis_txn": str(pool_genesis_txn_path)})
+
+    #Set protocol version 2
+    await pool.set_protocol_version(PROTOCOL_VERSION)
+
+    try:
+        await pool.create_pool_ledger_config(pool_name, pool_config)
+    except IndyError as ex:
+        if ex.error_code == ErrorCode.PoolLedgerConfigAlreadyExistsError:
+            pass
+
+    pool_handle = await pool.open_pool_ledger(pool_name, None)
+
+    logger.info("==============================")
+
 
 if __name__ == '__main__':
     run_coroutine(run)

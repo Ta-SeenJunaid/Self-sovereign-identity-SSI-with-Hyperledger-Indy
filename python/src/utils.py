cef091aba7de420935742e189d5b58f56f484e3e
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-06-09 19:45:11 +0600

    ensure_previous_request_applied

--- python/src/utils.py
+++ python/src/utils.py
@@ -1,6 +1,10 @@
 import asyncio
 from os import environ
 from pathlib import Path
+import json
+import time
+
+from  indy import ledger
 
 PROTOCOL_VERSION = 2
 
@@ -44,3 +48,14 @@ def run_coroutine(coroutine, loop=None):
     if loop is None:
         loop = asyncio.get_event_loop()
     loop.run_until_complete(coroutine())
+
+
+async def ensure_previous_request_applied(pool_handle, checker_request, checker):
+    for _ in range(3):
+        response = json.loads(await ledger.submit_request(pool_handle, checker_request))
+        try:
+            if checker(response):
+                return json.dumps(response)
+        except TypeError:
+            pass
+        time.sleep(5)

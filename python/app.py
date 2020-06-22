6fd2fe79e948d477706a4ba1dca8350b05d2f0ef
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-06-22 10:35:32 +0600

    checking for custom wallet storage

--- python/app.py
+++ python/app.py
@@ -1,5 +1,7 @@
 import logging
 import argparse
+import sys
+from ctypes import *
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -13,3 +15,28 @@ parser.add_argument('-s', '--creds', help='entry point for dynamic library')
 
 args = parser.parse_args()
 
+# checking for custom wallet storage
+if args.storage_type:
+    if not (args.library and args.entrypoint):
+        parser.print_help()
+        sys.exit(0)
+    stg_lib =CDLL(args.library)
+    result = stg_lib[args.entrypoint]()
+    if result != 0:
+        print("Error unable to load wallet storage", result)
+        parser.print_help()
+        sys.exit(0)
+
+    # for postgres storage, also call the storage init (non-standard)
+    if args.storage_type == "postgres_storage":
+        try:
+            print("Calling init_storagetype() for postgres:", args.config, args.creds)
+            init_storagetype = stg_lib["init_storagetype"]
+            c_config = c_char_p(args.config.encode('utf-8'))
+            c_credentials = c_char_p(args.creds.encode('utf-8'))
+            result = init_storagetype(c_config, c_credentials)
+            print(" ... returns ", result)
+        except RuntimeError as e:
+            print("Error initializing storage, ignoring ...", e)
+
+    print("Success, loaded wallet storage", args.storage_type)
